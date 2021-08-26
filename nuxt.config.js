7ff9d191438d7eb69a582304d3969998a7268874
Ilya Berzhanin <IlyaBerzhanin@coherentsolutions.com>
2021-08-26 11:01:30 +0300

    feat(config): config stylelint

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
